48d5e59398a9c70c36f5a6349ca52a9637d74923
Deepak Suthar <codewithdeepak@yahoo.com>
2022-02-26 14:03:07 +0530

    fixed multer

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,10 @@
 import { Module } from '@nestjs/common'
-import { MulterModule } from '@nestjs/platform-express'
-import { diskStorage } from 'multer'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { DetectionModule } from './services/detection/detection.module'
-import { imageFileFilter } from './utils/image-filter'
 
 @Module({
-  imports: [
-    DetectionModule,
-    MulterModule.register({
-      storage: diskStorage({ destination: '../uploads' }),
-      fileFilter: imageFileFilter,
-    }),
-  ],
+  imports: [DetectionModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/services/detection/detection.controller.ts
+++ src/services/detection/detection.controller.ts
@@ -1,5 +1,8 @@
-import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common'
+import { Controller, HttpStatus, Post, UploadedFile, UseInterceptors } from '@nestjs/common'
 import { FileInterceptor } from '@nestjs/platform-express'
+import { memoryStorage } from 'multer'
+import { HttpError } from 'src/utils/error'
+import { imageFileFilter } from 'src/utils/image-filter'
 import { DetectionService } from './detection.service'
 
 @Controller('detection')
@@ -7,10 +10,20 @@ export class DetectionController {
   constructor(private readonly detectionService: DetectionService) {}
 
   @Post('upload')
-  @UseInterceptors(FileInterceptor('file'))
-  uploadSingle(
+  @UseInterceptors(
+    FileInterceptor('file', {
+      storage: memoryStorage(),
+      fileFilter: imageFileFilter,
+    })
+  )
+  async uploadSingle(
     @UploadedFile() file: Express.Multer.File
   ): Promise<(Uint8Array | Float32Array | Int32Array)[]> {
+    if (file === undefined) {
+      throw HttpError(HttpStatus.PRECONDITION_FAILED, 'File parameter is missing')
+      return null
+    }
+
     return this.detectionService.detectImage(file)
   }
 }
